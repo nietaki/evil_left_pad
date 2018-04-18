0f871e30b9bba151e1357fe807fdf5e2199a8edc
Jacek Królikowski <nietaki@gmail.com>
2018-04-18 11:36:42 +0100

    Facilitate the docs for the release

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule EvilLeftPad.MixProject do
       elixir: "~> 1.4",
       package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/nietaki/evil_left_pad",
+      docs: docs(),
     ]
   end
 
@@ -22,6 +24,7 @@ defmodule EvilLeftPad.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps() do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, optional: true, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
@@ -45,5 +48,13 @@ defmodule EvilLeftPad.MixProject do
 
     See `hoplon` package for details
     """
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://github.com/nietaki/evil_left_pad",
+      extras: ["README.md"],
+    ]
   end
 end
