feb3a6d7cdbddbf231017171fd622db85d39f285
Jacek Królikowski <nietaki@gmail.com>
2018-04-18 21:37:24 +0100

    A fancy new implementation using :binary and no recursion!

--- lib/evil_left_pad.ex
+++ lib/evil_left_pad.ex
@@ -1,11 +1,13 @@
 defmodule EvilLeftPad do
 
+  @doc """
+  Pads the beginning of a given string with spaces until it's no
+  longer than `count` characters.
+  """
   def left_pad(string, count) do
-    if String.length(string) < count do
-      left_pad(" " <> string, count)
-    else
-      string
-    end
+    padding_length = max(count - String.length(string), 0)
+    padding = :binary.copy(" ", padding_length)
+    padding <> string
   end
 
 end
