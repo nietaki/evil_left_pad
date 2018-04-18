646b64e3aac7ff02faf1bec9636259c1e015cfbf
Jacek Królikowski <nietaki@gmail.com>
2018-04-18 21:25:11 +0100

    Add some tests

--- test/evil_left_pad_test.exs
+++ test/evil_left_pad_test.exs
@@ -2,6 +2,21 @@ defmodule EvilLeftPadTest do
   use ExUnit.Case
   doctest EvilLeftPad
 
-  # TODO add some tests
+  import EvilLeftPad
 
+  test "when desired length is more than the string length" do
+    assert "  foo" == left_pad("foo", 5)
+  end
+
+  test "when desired length == string length" do
+    assert "foobar" == left_pad("foobar", 6)
+  end
+
+  test "when desired length is saller than string length" do
+    assert "foobar" == left_pad("foobar", 3)
+  end
+
+  test "behaves correctly on UTF8 strings" do
+    assert "  łóż" == left_pad("łóż", 5)
+  end
 end
